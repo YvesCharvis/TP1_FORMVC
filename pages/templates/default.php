2106f610e639c6ac15680edce131049f39ee0c6c
John DoeYvesCharvis <charvisyves@ymail.com>
2017-03-22 15:47:55 +0100

    first commit

---
+++ pages/templates/default.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    	<!-- Bootstrap core CSS -->
+    	<link href="frameworks/bootstrap.min.css" rel="stylesheet">
+    	<link rel="stylesheet" type="text/css" href="css/styles.css">
+  </head>
+<body>
+
+
+
+</body>
+</html>
