cfd2139bba5a1ca8d23da58a7b9b9abce8df80fd
John DoeYvesCharvis <charvisyves@ymail.com>
2017-03-23 11:21:00 +0100

    stylesv1

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -19,36 +19,40 @@
 
 <div class="Formulaire">
 <div class="row">
-    <form class="col s6">
-    <div class="row">
-    	<div class="input-field col s6">
-			<label class=""><input type="text" class="validate" name="nom" placeholder="Nom"></label> 
-		</div>
-		<div class="input-field col s6">		
-			<label class=""><input type="text" class="validate" name="prenom" placeholder="Prenom"></label>
-		</div>
+		<form class="col s6">
+		    <div class="row">
+		    	<div class="input-field col s6">
+					<label class=""><input type="text" class="validate" name="nom" placeholder="Nom"></label> 
+				</div>
+				<div class="input-field col s6">		
+					<label class=""><input type="text" class="validate" name="prenom" placeholder="Prenom"></label>
+				</div>
+			</div>
+			<div class="row">
+		 		   	<div class="input-field col s6">
+					<label class=""><input type="date" class="form-control" name="naissance"></label>
+				</div>
+			</div>
+			<div class="row">
+		        <div class="input-field col s6">	
+					<label class=""><input type="text" class="form-control" name="adresse" placeholder="Adresse"></label>
+				</div>
+		        <div class="input-field col s6">	 	
+					<label class="">  <input type="number" class="form-control" name="postal" placeholder="Code Postale"></label>
+				</div>
+			</div>	 
+			<div class="row">
+		        <div class="input-field col s4">		
+					<label class=""><input type="number" class="form-control" name="tel" placeholder="Numéro de téléphone"></label>
+				</div>
+			</div>
+				<div class="flex dark">
+ 		 			<a href="#0" class="bttn-dark">Continue</a>
+				</div>
+		</form>
+	<div class="col s6">
+		<img class="col s12" src="img/pacman.png">
 	</div>
-	<div class="row">
-    	<div class="input-field col s6">
-			<label class=""><input type="date" class="form-control" name="naissance"></label>
-		</div>
-	</div>
-	<div class="row">
-        <div class="input-field col s6">	
-			<label class=""><input type="text" class="form-control" name="adresse" placeholder="Adresse"></label>
-		</div>
-        <div class="input-field col s6">	 	
-			<label class="">  <input type="number" class="form-control" name="postal" placeholder="Code Postale"></label>
-		</div>
-	</div>	 
-	<div class="row">
-        <div class="input-field col s4">		
-			<label class=""><input type="number" class="form-control" name="tel" placeholder="Numéro de téléphone"></label>
-		</div>
-	</div>
-</form>
-<div class="col s6">
-	<img class="col s12" src="img/pacman.png">
 </div>
 </div>
 
