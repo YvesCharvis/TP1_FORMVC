31038721e92b96798662b380c5471e6c39d3c583
John DoeYvesCharvis <charvisyves@ymail.com>
2017-03-28 09:16:08 +0200

    login_root

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
 
-	"db_user" => "live",
-	"db_pass" => "live",
-	"db_name" => "newapp",
+	"db_user" => "root",
+	"db_pass" => "",
+	"db_name" => "correct_tp1",
 	"db_host" => "localhost"
 
 	);
